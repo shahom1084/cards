46695ab873f87085c8eb53a47ae2f0a953fa8cd9
Om Shah <omshah429@gmail.com>
2025-10-11 19:35:28 +0530

    defined new (deck,game,players) classes

--- deck.py
+++ deck.py
@@ -1,17 +1,20 @@
+from dataclasses import dataclass, field
 import random
-class Carddeck:
-    def __init__(self):
-        self.cards = []
-        self.suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades']
-        self.ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace']
+
+@dataclass
+class CardDeck:
+    suits: tuple[str, ...] = ('Hearts', 'Diamonds', 'Clubs', 'Spades')
+    ranks: tuple[str, ...] = ('2', '3', '4', '5', '6', '7', '8', '9', '10',
+                              'Jack', 'Queen', 'King', 'Ace')
+    cards: list[tuple[str, str]] = field(default_factory=list)
+
     def create_deck(self):
         self.cards = [(rank, suit) for suit in self.suits for rank in self.ranks]
         return self.cards
+
     def shuffle_deck(self):
         random.shuffle(self.cards)
         return self.cards
 
-deck = Carddeck()
-deck.create_deck()
-cards=deck.shuffle_deck()
-print(cards)
+    def draw_card(self):
+        return self.cards.pop() if self.cards else None
